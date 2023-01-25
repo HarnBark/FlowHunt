97c5b69da1b3393348cf8ada99a7d6e56048d6c1
Vasilev Artyom <barkqaqa@gmail.com>
2023-01-25 18:31:39 +0300

    add ssr and main router

---
+++ app.js
@@ -0,0 +1,23 @@
+require('@babel/register');
+
+const express = require('express');
+const db = require('../db/models');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const mainRouter = require('./routes/main.routes');
+
+app.use('/', mainRouter);
+
+const start = async () => {
+  try {
+    await db.sequelize.authenticate();
+    app.listen(PORT, () => {
+      console.log(`Сервер слушает ${PORT} порт`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+start();
