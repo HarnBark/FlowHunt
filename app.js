f14f30be66e53568e0d5f7017764510048c81604
Vasilev Artyom <barkqaqa@gmail.com>
2023-01-25 20:03:48 +0300

    config ready

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 require('@babel/register');
 
 const express = require('express');
-const db = require('../db/models');
+const db = require('./db/models');
+const config = require('./config/config')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const mainRouter = require('./routes/main.routes');
 
+config(app);
+
 app.use('/', mainRouter);
+
 
 const start = async () => {
   try {
