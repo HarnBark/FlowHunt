78b1ddda2bce13d6a7a8ec49447439e93f6b2dd5
jkantyukova <jkantyukova@gmail.com>
2023-01-25 20:21:03 +0300

    Fix map function

--- views/Main.jsx
+++ views/Main.jsx
@@ -2,11 +2,11 @@ const React = require('react');
 const Layout = require('./Layout');
 const CandidatesList = require('./CandidatesList');
 
-function Main({ title }) {
+function Main({ title, candidatesDB }) {
   return (
     <Layout title={title}>
       <h1>Main page</h1>
-      <CandidatesList />
+      <CandidatesList candidatesDB={candidatesDB} />
     </Layout>
   );
 }
