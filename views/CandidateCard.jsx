55ca84cc2cafc7b4aa75e21a416771bd67d04bc9
Tanya <alisaeva4498@mail.ru>
2023-01-25 18:38:20 +0300

    candCard is made

---
+++ views/CandidateCard.jsx
@@ -0,0 +1,34 @@
+const React = require('react');
+
+function CandidateCard({ candidate, user }) {
+  return (
+    <div className="cand__profile__container">
+      <div className="cand__container">
+        <img src={candidate.img} alt="candidate" />
+        <div>{candidate.name}</div>
+        <div>{candidate.email}</div>
+        <div>{candidate.experience}</div>
+        <div>{candidate.phone}</div>
+        <div>{candidate.skype}</div>
+        <div>{candidate.zoom}</div>
+        <div>{candidate.status}</div>
+        <select>
+          <option>Сменить этап</option>
+          <option>Отправлено приглашение</option>
+          <option>Звонок-скрининг</option>
+          <option>Видеоинтервью</option>
+          <option>Передано заказчику</option>
+          <option>Интервью с заказчиком</option>
+          <option>Оффер</option>
+          <option>Отказ</option>
+        </select>
+        {/* {user && user.id === candidate.user_id && (
+          <button type="button" className="prodano" data-id={car.id}>
+            продал
+          </button>
+        )} */}
+      </div>
+    </div>
+  );
+}
+module.exports = CandidateCard;
